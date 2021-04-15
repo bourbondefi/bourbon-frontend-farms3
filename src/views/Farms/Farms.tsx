8433cc2a021396642b56064b14fdb1cd77b1bbcc
bourbondefi <80276717+bourbondefi@users.noreply.github.com>
2021-04-15 03:18:04 -0400

    fix description

--- src/views/Farms/Farms.tsx
+++ src/views/Farms/Farms.tsx
@@ -94,13 +94,13 @@ const Farms: React.FC<FarmsProps> = (farmsProps) => {
       <Heading as="h1" size="lg" color="primary" mb="50px" style={{ textAlign: 'center' }}>
         {
           tokenMode ?
-            TranslateString(10002, 'Stake tokens to earn Rare Bourbon Tokens')
+            TranslateString(10002, 'Stake tokens to earn Top Shelf Bournon Tokens')
             :
-          TranslateString(320, 'Stake LP tokens to earn Rare Bourbon Tokens')
+          TranslateString(320, 'Stake LP tokens to earn Top Shelf Bourbon Tokens')
         }
       </Heading>
       <Heading as="h2" color="secondary" mb="50px" style={{ textAlign: 'center' }}>
-        {TranslateString(10000, 'Deposit Fee will be used to buyback Rare Bourbon Tokens')}
+        {TranslateString(10000, 'Deposit Fee will be used to buyback Top Shelf Bourbon Tokens')}
       </Heading>
       <FarmTabButtons stakedOnly={stakedOnly} setStakedOnly={setStakedOnly}/>
       <div>
